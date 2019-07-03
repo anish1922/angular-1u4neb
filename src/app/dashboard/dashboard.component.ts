b5eaf3096d409afdc66d60767d38b158f8937396
anish1922 <anish_1922@yahoo.co.in>
2019-07-03 23:29:02 +0530

    1 file modified, 3 added

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {HeroserviceService} from'../heroservice.service'
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+heroes;
+heroService;
+  constructor(heroService:HeroserviceService) { 
+    this.heroService=HeroserviceService;
+  }
+
+  ngOnInit() {
+  }
+getHeroes(): void {
+  this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+}
+}
